8f9a17328ab6f1a49c6502f1386ec86d7b6709ca
Klychnikov Alexandr <klychnikov75@mail.ru>
2022-12-17 15:58:45 +0300

    init: start TaskMaster project, setting up the environment, create favicon

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './styles/theme';
+import './index.css';
+import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename="/taskmaster">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
