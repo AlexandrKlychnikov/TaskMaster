96e09c3292535938b39e4d942752f35ceac1b098
Klychnikov Alexandr <klychnikov75@mail.ru>
2022-12-25 09:49:21 +0300

    feat: create app store

--- src/pages/Boards.tsx
+++ src/pages/Boards.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 export const Boards = () => {
-  return <div>Boards</div>;
+  return (
+    <Container
+      fixed
+      sx={{
+        height: '90vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Typography variant="h2">Boards</Typography>
+    </Container>
+  );
 };
